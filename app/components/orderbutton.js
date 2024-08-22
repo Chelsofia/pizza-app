eaad0e8e26871dd849f8b9f195af7301471ac577
chelsofia <rachelasse01@gmail.com>
2024-08-22 19:38:32 +0100

    update

--- app/components/orderbutton.js
+++ app/components/orderbutton.js
@@ -5,7 +5,7 @@ import { FaShoppingCart } from "react-icons/fa";
 export default function OrderButton() {
   return (
     <Link
-      href="/users"
+      href="/products"
       className="inline-flex items-center cursor-pointer bg-[#FBB200] px-6 py-3 text-lg rounded-full tracking-wider font-medium text-white gap-4"
     >
       <FaShoppingCart />
